b740b026f93209dd70dfe6fc234151516c1e2923
saintofidiocy <saintofidiocy@users.noreply.github.com>
2022-03-12 18:26:01 -0700

    Removed a leftover #include

--- Loading Hook/loading_hook.cpp
+++ Loading Hook/loading_hook.cpp
@@ -1,12 +1,12 @@
 #include "loading_hook.h"
 #include <SCBW/api.h>
-#include <SCBW/Storm.h>
 #include <cstdio>
 
 namespace hooks {
 
 // Called the end of the data loading function, after GRPs and such are loaded
 void loadingData() {
+  // load some mpq files, or whatever
   
 }
 
